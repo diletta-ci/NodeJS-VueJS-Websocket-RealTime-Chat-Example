73b3ce589ca92ae8a117a1be73667317b9907c34
Diletta Cala Impirotta <diletta.impirotta@huumun.com>
2020-05-25 16:02:03 +0200

    Socket connection on server

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var socket = require('socket.io');
 
 // App setup
 var app = express();
@@ -8,3 +9,10 @@ var server = app.listen(4000, function() {
 
 // Static file
 app.use(express.static('public'));
+
+// Socket setup
+var io = socket(server);
+
+io.on('connection', function(socket) {
+    console.log('Socket connected', socket.id);
+});
